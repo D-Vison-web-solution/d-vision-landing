dd5239eed10d41f2bc9fd09290b091dc43772788
damon_gege <z3439119@gmail.com>
2023-09-14 15:00:38 +1000

    update blog list page

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,7 +9,18 @@ const showcase = defineCollection({
     featured: z.number().min(1).optional(),
   }),
 });
+const blog = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string().min(1),
+    image: z.string(),
+    description: z.string(),
+    date: z.string(),
+    featured: z.number().min(1).optional(),
+  }),
+});
 
 export const collections = {
   showcase,
+  blog
 };
